626c6605d1695b65d7d650ac7dc61245a8290873
Saurabh <saurabh.nit20@gmail.com>
2020-11-10 09:16:17 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee PayRoll Using Threads");
+        }
+    }
+
+}
